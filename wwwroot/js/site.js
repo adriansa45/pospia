6841c0b021e601704ad173caa7deb4f63c73129f
adriansa45 <73455169+adriansa45@users.noreply.github.com>
2022-11-22 22:00:17 -0600

    avances

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -35,7 +35,28 @@ function QuitarObjeto(id) {
 function ImprimirProductos() {
     carrito.html('');
     productos.forEach(function (e) {
-        let htmlstring = '<div class="card" style="max-height:20rem" ><div class="card-header">' + e.Name + '</div><div class="card-body"><img style="max-height: 10vh;" src = "/imgs/coca.webp"/>'
+        let htmlstring = ` <div class="item">
+            <div class="info">
+                <div>
+                    <p class="name">`+e.Name+`</p>
+                    <div>
+                        <p class="price">`+e.Price+`</p>
+                        <div class="btns">
+                            <input class="less" type="button"  onclick = "QuitarObjeto(` + e.ProductId + `)" value="-"/>
+                            <input class="number" type="number" name="" id="" value="`+ e.Amount+`"/>
+                            <input class="plus" type="button" onclick = "AñadirObjeto(` + e.ProductId + `)" value="+"/>
+                        </div>
+                    </div>
+                </div>
+                <div class="total">
+                    <p>`+ e.Price +`</p>
+                </div>  
+
+            </div>
+        </div>` 
+         
+
+        let htmlstring2 = '<div class="card" style="max-height:20rem" ><div class="card-header">' + e.Name + '</div><div class="card-body"><img style="max-height: 10vh;" src = "/imgs/coca.webp"/>'
             + '</div><div class="card-footer form-group text-dark" ><div class="form-group" style="display: flex;">'
             + '<button id="4" class="btn btn-primary" onclick = "AñadirObjeto(' + e.ProductId + ')" > + </button><input class="form-control" value="' + e.Amount
             + '"/><button id="4" class="btn btn-danger" onclick = "QuitarObjeto(' + e.ProductId + ')" > - </button></div></div></div>';
